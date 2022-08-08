a61ca7f85f4f58b9ac4ef074fc99fcacb1cb9be6
Mitchell Foote <mcfoote321@gmail.com>
2022-08-08 16:47:43 -0400

    Adding js for quiz questions.

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+
+var qIndex = 0;
+var score = 0;
+
+var questions = [
+    
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ___.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ___.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ___ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<javascript>", "<script>", "<js>", "<scripting>"],
+        answer: "<script>"
+    },
+    {
+        title: "JavaScript is a ___ -side programming language.",
+        choices: ["Client", "Server", "Both", "None"],
+        answer: "Both"
+
+    },
+    {
+        title: "How do you find the minimum of x and y using JavaScript?",
+        choices: ["min(x,y)", "Math.min(x,y)", "Math.min(xy)", "min(xy)"],
+        answer: "Math.min(x,y)"
+    },
+
+];
